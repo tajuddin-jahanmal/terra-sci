7338b6eb26b0936e3bc6e941a84c1db93c1a8625
tajuddin-jahanmal <qaderitajuddin@gmail.com>
2026-03-08 16:59:05 +0430

    tiny changes in JS

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 (function() {
     const cards = document.querySelectorAll('[data-glow]');
     cards.forEach(card => {
-    card.addEventListener('mousemove', function(e) {
-        const rect = this.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        this.style.setProperty('--x', x + 'px');
-        this.style.setProperty('--y', y + 'px');
-    });
+        card.addEventListener('mousemove', function(e) {
+            const rect = this.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            this.style.setProperty('--x', x + 'px');
+            this.style.setProperty('--y', y + 'px');
+        });
     });
 
     const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll(".nav-links a");
 
     window.addEventListener("scroll", () => {
-    let current = "";
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop - 200;
-        if (pageYOffset >= sectionTop) {
-        current = section.getAttribute("id");
-        }
-    });
+        let current = "";
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 200;
+            if (pageYOffset >= sectionTop) {
+                current = section.getAttribute("id");
+            }
+        });
 
-    navLinks.forEach(link => {
-        link.style.color = "#cbd5e0";
-        link.style.textShadow = "none";
-        if (link.getAttribute("href") === `#${current}`) {
-        link.style.color = "#b3ffe0";
-        }
-    });
+        navLinks.forEach(link => {
+            link.style.color = "#cbd5e0";
+            link.style.textShadow = "none";
+            if (link.getAttribute("href") === `#${current}`) {
+                link.style.color = "#b3ffe0";
+            }
+        });
     });
 })();
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if(target) target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target) target.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
 });
 
